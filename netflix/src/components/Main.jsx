a06cd2472eec47a7b4b8661358468d959443f094
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-26 20:47:51 +0200

    did format file.

--- netflix/src/components/Main.jsx
+++ netflix/src/components/Main.jsx
@@ -1,25 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import requests from '../Request';
+import React, { useEffect, useState } from "react";
+import requests from "../Request";
 import axios from "axios";
 const Main = () => {
-    const [movies,setMovies] = useState([]);
-    const movie = movies.at(Math.floor(Math.random() * movies.length))
+  const [movies, setMovies] = useState([]);
+  const movie = movies.at(Math.floor(Math.random() * movies.length));
 
-    useEffect(() => {
-          axios.get(requests.requestPopular).then((response) => {
-              setMovies(response.data.results)
-          })
-    },[])
-         console.log(movie);
-         
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+  console.log(movie);
+
   return (
-    <div className='w-full h-[550px] text-white'>
-          <div className='w-full h-full'>
-            <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
-                <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-          </div>     
-    </div>
-  )
-}
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
 
-export default Main
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="font-bold text-2xl md:text-5xl">{movie?.title}</h1>
+          <button className="border bg-gray-300 text-black border-gray-300 py-3 px-5">
+            Play
+          </button>
+          <button className="ml-4 border  text-white py-3 px-5">
+            Watch it later
+          </button>
+
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
