7768c0cf42f318f701fbdf99948522f00db79375
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-27 16:31:59 +0200

    did it right.

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -27,7 +27,7 @@ const Row = ({ title, fetchURL }) => {
                 src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
                 alt={item.title}
               />
-              <div className="absolute top-0 left-9  h-full w-full  opacity-0 hover:opacity-100 text-white">
+              <div className="absolute top-0 left-0  h-full w-full  hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="flex justify-center items-center h-full white-space-normal text-xs md:text-sm font-bold">
                   {item?.title}
                 </p>
