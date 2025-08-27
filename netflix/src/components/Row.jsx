d02613846be34aae2f8ed6d382cdd7458f0436dc
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-27 16:21:14 +0200

    made file-format.

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -1,38 +1,44 @@
-import axios from 'axios'
-import {FaHeart,FaRegHeart} from 'react-icons/fa'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
 
-const Row = ({title, fetchURL}) => {
-    const [movies, setMovies] = useState([])
-    const [like , setLike] = useState(false)
-    useEffect(() => {
-        axios.get(fetchURL).then((response) => {
-            setMovies(response.data.results)
-        })
-    },[fetchURL])
- console.log(movies);
- 
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false);
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+  console.log(movies);
+
   return (
     <div>
-          <h2 className='font-bold text-white md:text=xl p-4'> {title} </h2>
-          <div className='relative flex item-center'>
-                   <div id={'slider'}>
-                         {movies.map((item,id) => (
-                            <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block'>
-                               <img  className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
-                                <div className='absolute top-0 left-9  h-full w-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                                    <p className='flex justify-center items-center h-full white-space-normal text-xs md:text-sm font-bold'>{item?.title}</p>
-                                  <p>
-                                    {like ? <FaHeart/> : <FaRegHeart/> }
-                                  </p>
-                                </div>
-                            </div>
-                         ))}
-                   </div>
-          </div>
+      <h2 className="font-bold text-white md:text=xl p-4"> {title} </h2>
+      <div className="relative flex item-center">
+        <div id={"slider"}>
+          {movies.map((item, id) => (
+            <div
+              key={id}
+              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block"
+            >
+              <img
+                className="w-full h-auto block"
+                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+                alt={item.title}
+              />
+              <div className="absolute top-0 left-9  h-full w-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <p className="flex justify-center items-center h-full white-space-normal text-xs md:text-sm font-bold">
+                  {item?.title}
+                </p>
+                <p>{like ? <FaHeart /> : <FaRegHeart />}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Row;
-    
