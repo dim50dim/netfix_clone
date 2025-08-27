b2aac5fd5f6a0ad6608e0b9d5d08fb0430de28ee
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-27 16:16:51 +0200

    added necessary key={} to map() , in React this is 100% obvious action.

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -16,8 +16,11 @@ const Row = ({title, fetchURL}) => {
           <div className='relative flex item-center'>
                    <div id={'slider'}>
                          {movies.map((item,id) => (
-                            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block'>
-                               <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
+                            <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block'>
+                               <img  className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
+                                <div className='absolute top-0 left-9  h-full w-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                                    <p className='flex justify-center items-center h-full white-space-normal text-xs md:text-sm font-bold'>{item?.title}</p>
+                                </div>
                             </div>
                          ))}
                    </div>
