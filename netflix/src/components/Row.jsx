26890201e0c1d6309dcdd6ff27ed4cd769312c99
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-28 14:38:35 +0200

    changed var slider to const slider.

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -14,11 +14,11 @@ const Row = ({ title, fetchURL,rowID }) => {
   }, [fetchURL]);
 
      const slideLeft = () => {
-         var slider = document.getElementById('slider' + rowID);
+         const  slider = document.getElementById('slider' + rowID);
          slider.scrollLeft= slider.scrollLeft - 500
      }
      const slideRight = () => {
-         var slider = document.getElementById('slider' + rowID);
+         const slider = document.getElementById('slider' + rowID);
          slider.scrollLeft= slider.scrollLeft + 500
      }
   return (
--- netflix/src/firebase.js
+++ netflix/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import.meta.env
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +23,5 @@ const firebaseConfig = {
   appId: import.meta.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+ export const app = initializeApp(firebaseConfig);
+ export const auth = getAuth(app);
