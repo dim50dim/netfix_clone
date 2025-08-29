d0c2e8175985bbf1496841205707ed4a3212f996
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-29 19:49:42 +0200

    auth works - well.

--- netflix/src/pages/Signup.jsx
+++ netflix/src/pages/Signup.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
-
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 const Signup = () => {
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const {user,signUp} = UserAuth()
+
+    const handleSubmit =async (e) => {
+        e.preventDefault();
+         try {
+            await signUp(email,password)
+         } catch (error) {
+            console.log(error);
+            
+         }
+    }
   return (
     <> 
     <div className='w-full h-screen'>
@@ -14,6 +28,24 @@ const Signup = () => {
             <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
                <div className="max-w-[320px] mx-auto py-16">
                     <h1 className='font-bold text-3xl '> Sign up</h1>
+                     <form onSubmit={handleSubmit} className='w-full flex flex-col py-4' >
+                        <input
+                        onChange={(e) => setEmail(e.target.value)}
+                         type="email" className='border-white p-3 my-3 bg-gray-600 rounded' placeholder='Email' autoComplete='email' />
+                        <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"className='border-white p-3 my-3 bg-gray-600 rounded' placeholder='Password' autoComplete='current-password' />
+                          <button className='bg-red-700 py-3 my-6 rounded font-bold'>Sign Up</button>
+                           <div className='flex justify-between items-center text-sm text-gray-400'>
+                                 <p > <input className='mr-2' type="checkbox" /> Remember  </p>
+                                 <p> Need Help ?</p>
+                           </div>
+                           <p className='py-8'> <span className='text-gray-600'>Already subscribed to Netflix???
+                              <Link to="/login">Sign in</Link>
+                           </span>{' '}
+                           
+                           </p>
+                     </form>
                </div>
             </div>
         </div>
