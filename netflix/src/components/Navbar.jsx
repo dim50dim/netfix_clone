597d6e3c5f3d036646a439a42a7fc82e4f2187fd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-30 09:52:51 +0200

    in Navbar implemented ternary operator.

--- netflix/src/components/Navbar.jsx
+++ netflix/src/components/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { UserAuth } from '../context/AuthContext'
 const Navbar = () => {
+  const {user,logOut} = UserAuth()
+  // console.log(user.email);
+     const handleLogOut = async () => {
+            try {
+              await logOut()
+            } catch (error) {
+              console.log(error);
+              
+            }
+     }  
   return (
     <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
       <Link to='/'>
           <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
           </Link>
-          <div>
+            {user?.email ?  <div>
+           <Link to='/account'> <button className='text-white pr-4'>Account</button></Link>
+          <Link to='/signup'>  <button className='bg-red-600 px-6 py-4 rounded cursor-pointer text-white'>Logout</button></Link>
+          </div> : 
+              <div>
            <Link to='/login'> <button className='text-white pr-4'>Sign in</button></Link>
           <Link to='/signup'>  <button className='bg-red-600 px-6 py-4 rounded cursor-pointer text-white'>Sign up</button></Link>
-          </div>
+          </div> }
     </div>
   )
 }
