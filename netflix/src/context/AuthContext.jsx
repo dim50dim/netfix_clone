3e17ea2c81452be471eba70f1e14872a7dff7013
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-30 13:19:56 +0200

    installed  -g firebase-tools

--- netflix/src/context/AuthContext.jsx
+++ netflix/src/context/AuthContext.jsx
@@ -1,13 +1,11 @@
 import {useContext, createContext,useEffect, useState} from 'react'
-import {auth} from "../firebase"
-
-
-
+import {auth,db} from "../firebase"
 import {
      createUserWithEmailAndPassword,
      signInWithEmailAndPassword,signOut,
      onAuthStateChanged 
     } from 'firebase/auth';
+import { setDoc,doc } from 'firebase/firestore';
 
     const AuthContext = createContext();
 
@@ -15,8 +13,11 @@ export function AuthContextProvider({children}) {
     const [user,setUser] = useState({});
 
      function signUp(email,password){
-        return createUserWithEmailAndPassword(auth,email,password)
-     }
+         createUserWithEmailAndPassword(auth,email,password)
+        setDoc(doc(db,'users',email),{
+            savedShows: []
+        })
+    }
      function logOut(){
         return signOut(auth)
      }
--- netflix/src/firebase.js
+++ netflix/src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // ← Add this
-
+import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +20,5 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
  export const auth = getAuth(app);
 export default app;
+ const db = getFirestore(app)
+ export {db};
